10239158a1c7d798e9d66ae6aa2c3a1ca3e391ca
matthew.earlywine <matthew.earlywine@AFSWWK47C9MT97.us.accenturefederal.com>
2022-08-23 16:36:50 -0500

    Spring Configuration using XML
    Using Setter Injection on branch "main".

--- src/main/java/com/pluralsight/main/Application.java
+++ src/main/java/com/pluralsight/main/Application.java
@@ -1,12 +1,20 @@
 package com.pluralsight.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 
 public class Application {
 	
 	public static void main (String args[]) {
-		SpeakerService service = new SpeakerServiceImpl();
+		
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		
+		//SpeakerService service = new SpeakerServiceImpl();
+		SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 		
 		System.out.println("This is test code");
 		System.out.println(service.findAll().get(0).getFirstName());
